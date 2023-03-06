49653c6ffec256dfb308b3739d5a248ecf7f7b3d
Vladimir RM <shvdrummer@gmail.com>
2023-03-06 20:11:34 +0200

    find el in arr done

--- script!.js
+++ script!.js
@@ -25,3 +25,25 @@
 // console.log(arr.indexOf('Vasya'))
 
 // //////////find////////////////////////////////
+
+
+// let arr = [
+//     {
+//       name: "Yana",
+//       age: 23,
+//     },
+//     {
+//       name: "Ivan",
+//       age: 22,
+//     },
+//     {
+//       name: "Peter",
+//       age: 21,
+//     },
+//   ];
+  
+//   let result = arr.find((item, i, arr) => {
+//     return item.age === 22;
+//   });
+  
+//   console.log(result);
--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let arr = ["Ivan ", "Mykola", "Yana"];
+let arr = [
+  {
+    name: "Yana",
+    age: 23,
+  },
+  {
+    name: "Ivan",
+    age: 22,
+  },
+  {
+    name: "Peter",
+    age: 21,
+  },
+];
 
+let result = arr.find((item, i, arr) => {
+  return item.age === 22;
+});
 
-
-
-
-
-console.log(arr.indexOf('Vasya'));
+console.log(result);
