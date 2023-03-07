4d19b026cd8f77263d8ef63bd4b4b0d11072c6c9
Vladimir RM <shvdrummer@gmail.com>
2023-03-07 08:13:01 +0200

    split srt in arr done

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let arr = [
-  {
-    name: "Yana",
-    age: 23,
-  },
-  {
-    name: "Ivan",
-    age: 22,
-  },
-  {
-    name: "Peter",
-    age: 21,
-  },
-];
+// let arr = [
+//   {
+//     name: "Yana",
+//     age: 23,
+//   },
+//   {
+//     name: "Ivan",
+//     age: 22,
+//   },
+//   {
+//     name: "Peter",
+//     age: 21,
+//   },
+// ];
 
-let result = arr.filter((item, i, arr) => {
-  return item.age >= 22;
-});
+let srt = "Ivan,Mykola,Stepan";
+
+let result = srt.split(",");
+
 console.log(result);
