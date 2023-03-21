1596c72aa7bf4e7bb259854602d9d93333280cb6
Vladimir RM <shvdrummer@gmail.com>
2023-03-21 10:31:15 +0200

    Find elem in arr

--- script.js
+++ script.js
@@ -11,28 +11,28 @@
 
 // let res = num.flat(1).reduce((acc, item) => acc + item);
 
-// let arr = [
-//   {
-//     name: "Yana",
-//     age: 23,
-//   },
-//   {
-//     name: "Ivan",
-//     age: 22,
-//   },
-//   {
-//     name: "Peter",
-//     age: 21,
-//   },
-// ];
+let arr = [
+  {
+    name: "Yana",
+    age: 23,
+  },
+  {
+    name: "Ivan",
+    age: 22,
+  },
+  {
+    name: "Peter",
+    age: 21,
+  },
+];
 // let arr = "ivan,stepan,petro";
 
-let arr = ["Yana", "Anna", "Mihalka"];
+// let arr = ["Yana", "Anna", "Mihalka"];
 
-let res = arr.includes("Anna");
+let res = arr.find((item) => {
+  return item.name === "Yana";
+});
 
 console.log(res);
-
-// console.log(res);
 
 // for( let i = 0; i< arr.length;i++){}
