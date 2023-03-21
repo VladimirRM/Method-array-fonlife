3cfb7ed027d92b009801d492582141c6e35e8a87
Vladimir RM <shvdrummer@gmail.com>
2023-03-21 10:23:04 +0200

    Delete item splice.

--- script.js
+++ script.js
@@ -11,28 +11,27 @@
 
 // let res = num.flat(1).reduce((acc, item) => acc + item);
 
-let arr = [
-  {
-    name: "Yana",
-    age: 23,
-  },
-  {
-    name: "Ivan",
-    age: 22,
-  },
-  {
-    name: "Peter",
-    age: 21,
-  },
-];
+// let arr = [
+//   {
+//     name: "Yana",
+//     age: 23,
+//   },
+//   {
+//     name: "Ivan",
+//     age: 22,
+//   },
+//   {
+//     name: "Peter",
+//     age: 21,
+//   },
+// ];
 // let arr = "ivan,stepan,petro";
 
-// let arr = ["Yana", "Anna", "Mihalka"];
+let arr = ["Yana", "Anna", "Mihalka"];
 
-res = arr.filter((item) => {
-  return item.age <= 21;
-});
-console.log(res);
+arr.splice(0, 2);
+
+console.log(arr);
 
 // console.log(res);
 
