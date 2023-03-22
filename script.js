9be6dd95270ef6a15f38e1cb5e3f29b86cf32dbe
Vladimir RM <shvdrummer@gmail.com>
2023-03-22 07:09:32 +0200

    Get obj using find.

--- script.js
+++ script.js
@@ -11,24 +11,26 @@
 
 // let res = num.flat(1).reduce((acc, item) => acc + item);
 
-// let arr = [
-//   {
-//     name: "Yana",
-//     age: 23,
-//   },
-//   {
-//     name: "Ivan",
-//     age: 22,
-//   },
-//   {
-//     name: "Peter",
-//     age: 21,
-//   },
-// ];
+let arr = [
+  {
+    name: "Yana",
+    age: 23,
+  },
+  {
+    name: "Ivan",
+    age: 22,
+  },
+  {
+    name: "Peter",
+    age: 21,
+  },
+];
 // let arr = "ivan,stepan,petro";
 
-let arr = ["Yana", "Anna", "Mihalka"];
-res = arr.includes("Anna");
+// let arr = ["Yana", "Anna", "Mihalka"];
+let res = arr.find((item) => {
+  return item.age === 21;
+});
 
 console.log(res);
 
