b8cee0331044bd11f7813181c0df12778dbb6c1a
Vladimir RM <shvdrummer@gmail.com>
2023-03-23 09:13:43 +0200

    Delete first letter in el.

--- script.js
+++ script.js
@@ -11,25 +11,27 @@
 
 // let res = num.flat(1).reduce((acc, item) => acc + item);
 
-// let arr = [
-//   {
-//     name: "Yana",
-//     age: 23,
-//   },
-//   {
-//     name: "Ivan",
-//     age: 22,
-//   },
-//   {
-//     name: "Peter",
-//     age: 21,
-//   },
-// ];
+let arr = [
+  {
+    name: "Yana",
+    age: 23,
+  },
+  {
+    name: "Ivan",
+    age: 22,
+  },
+  {
+    name: "Peter",
+    age: 21,
+  },
+];
 // let arr = "ivan,stepan,petro";
 
-let arr = ["Yana", "Anna", "Mihalka"];
+// let arr = ["Yana", "Anna", "Mihalka"];
 
-let res = arr.slice(0, 2);
+let res = arr.find((item) => {
+  return item.age === 21;
+});
 
 console.log(res);
 
