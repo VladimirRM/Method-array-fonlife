fad4ae820be3d2044cc4e5d7176ed18ecbfeeec9
Vladimir RM <shvdrummer@gmail.com>
2023-04-01 20:10:27 +0300

    Find el in obj.

--- script.js
+++ script.js
@@ -13,23 +13,25 @@
 
 // let res = num.flat(1).reduce((acc, item) => acc + item);
 
-// let arr = [
-//   {
-//     name: "Yana",
-//     age: 23,
-//   },
-//   {
-//     name: "Ivan",
-//     age: 22,
-//   },
-//   {
-//     name: "Peter",
-//     age: 21,
-//   },
-// ];
-let arr = ["Yana", "Anna", "Mihalka"];
+let arr = [
+  {
+    name: "Yana",
+    age: 23,
+  },
+  {
+    name: "Ivan",
+    age: 22,
+  },
+  {
+    name: "Peter",
+    age: 21,
+  },
+];
+// let arr = ["Yana", "Anna", "Mihalka"];
 
-let res = arr.includes("Yana");
+let res = arr.find((i) => {
+  return i.age === 21;
+});
 // };
 
 // let arr = "ivan, stepan, petro";
