70d5a06ad3edf3a1f6de56b00788451f4fba15d5
Vladimir RM <shvdrummer@gmail.com>
2023-04-05 08:56:44 +0300

    Add new item in arr.

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ let arr = [
 ];
 // let arr = ["Yana", "Anna", "Mihalka"];
 
-let res = arr.splice(0, 1);
-console.log(res);
+arr.splice(0, 0, "Ivan");
+console.log(arr);
 
 // for( let i = 0; i< arr.length;i++){}
